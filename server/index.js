3d357dc0694affdf197963a71e05ad326aa7929c
Sahil Gajanan Patil <sahilgpatil0007@gmail.com>
2025-08-25 15:32:37 +0530

    Add /ping

--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
 require("dotenv").config();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/courses", require("./routes/course"));
 app.use("/api/topics", require("./routes/topic"));
 app.use("/api/topic-content", require("./routes/topicContent"));
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const pool = require("./db");
+app.get("/ping", async (req, res) => {
+  try {
+    await pool.query("SELECT 1");
+    res.send("pong");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("error");
+  }
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
