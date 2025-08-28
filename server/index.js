a78384ed3267d0057bd2397bd90bbae5273cc0c0
Sahil Gajanan Patil <123616352+Sahil982480442@users.noreply.github.com>
2025-08-28 17:45:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,17 +4,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ["https://sanjivani-encyclopedia.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"], 
-    credentials: true,
-  })
-);
-
-app.options("*", cors());
-
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -22,6 +12,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/courses", require("./routes/course"));
 app.use("/api/topics", require("./routes/topic"));
 app.use("/api/topic-content", require("./routes/topicContent"));
+
 
 const pool = require("./db");
 app.get("/ping", async (req, res) => {
@@ -33,6 +24,7 @@ app.get("/ping", async (req, res) => {
     res.status(500).send("error");
   }
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
